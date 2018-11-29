799b67b7cd7587ea141d32fa6424507662f504c1
wjkang <977865769@qq.com>
2018-11-29 18:34:07 +0800

    update

---
+++ src/pages/sys/menu/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <d2-container>
+    <template slot="header">菜单</template>
+    Hello World
+    <template slot="footer">footer</template>
+  </d2-container>
+</template>
+
+<script>
+export default {
+  name: 'menu'
+}
+</script>
