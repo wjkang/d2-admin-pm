592925629497be82dd4dda69123fd2a689ce394d
wjkang <977865769@qq.com>
2018-12-19 18:18:13 +0800

    update

--- src/pages/permission/role/roleUser.vue
+++ src/pages/permission/role/roleUser.vue
@@ -1,51 +1,155 @@
 <template>
-    <el-dialog :visible.sync="dialogVisible" @open="dialogOpen">
-        <div slot="title">维护<el-tag>{{role.name}}</el-tag>用户</div>
-        <el-form :inline="true" :model="searchForm" ref="searchForm" size="mini" style="margin-bottom: -18px;">
-            <el-form-item label="名称" prop="name">
-                <el-input v-model="searchForm.name" placeholder="名称" style="width: 100px;" />
-            </el-form-item>
+  <el-dialog
+    :visible.sync="dialogVisible"
+    @open="dialogOpen"
+  >
+    <div slot="title">维护<el-tag>{{role.name}}</el-tag>用户</div>
+    <el-form
+      :inline="true"
+      :model="searchForm"
+      ref="searchForm"
+      size="mini"
+      style="margin-bottom: -18px;"
+    >
+      <el-form-item
+        label="名称"
+        prop="name"
+      >
+        <el-input
+          v-model="searchForm.name"
+          placeholder="名称"
+          style="width: 100px;"
+        />
+      </el-form-item>
 
-            <el-form-item label="邮箱" prop="email">
-                <el-input v-model="searchForm.email" placeholder="邮箱" style="width: 120px;" />
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="handleSearchFormSubmit">
-                    <d2-icon name="search" /> 查询
-                </el-button>
-            </el-form-item>
+      <el-form-item
+        label="邮箱"
+        prop="email"
+      >
+        <el-input
+          v-model="searchForm.email"
+          placeholder="邮箱"
+          style="width: 120px;"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+          @click="handleSearchFormSubmit"
+        >
+          <d2-icon name="search" /> 查询
+        </el-button>
+      </el-form-item>
 
-            <el-form-item>
-                <el-button @click="handleSearchFormReset">
-                    <d2-icon name="refresh" /> 重置
-                </el-button>
-            </el-form-item>
-        </el-form>
+      <el-form-item>
+        <el-button @click="handleSearchFormReset">
+          <d2-icon name="refresh" /> 重置
+        </el-button>
+      </el-form-item>
+    </el-form>
 
-        <el-table :data="tableData" v-loading="loading" size="small" stripe highlight-current-row style="width: 100%;" @sort-change="handleSortChange">
-            <el-table-column label="名称" prop="name">
-                <template slot-scope="scope">
-                    {{scope.row.name}}
-                </template>
-            </el-table-column>
+    <el-table
+      :data="tableData"
+      v-loading="loading"
+      size="small"
+      stripe
+      highlight-current-row
+      style="width: 100%;"
+      @sort-change="handleSortChange"
+    >
+      <el-table-column
+        label="账号名称"
+        prop="name"
+      >
+        <template slot-scope="scope">
+          {{scope.row.name}}
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="用户名称"
+        prop="trueName"
+      >
+        <template slot-scope="scope">
+          {{scope.row.trueName}}
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="邮箱"
+        prop="email"
+        sortable="custom"
+        :show-overflow-tooltip="true"
+      >
+        <template slot-scope="scope">
+          {{scope.row.email}}
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="phone"
+        prop="phone"
+      >
+        <template slot-scope="scope">
+          {{scope.row.phone}}
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="状态"
+        prop="isAdd"
+      >
+        <template slot-scope="scope">
+          <el-tag
+            v-if="scope.row.isAdd==2"
+            size="mini"
+            type="info"
+          >未添加</el-tag>
+          <el-tag
+            v-if="scope.row.isAdd==1"
+            size="mini"
+            type="success"
+          >已添加</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        align="center"
+      >
+        <template slot-scope="scope">
+          <el-button
+            v-if="scope.row.isAdd==2"
+            type="primary"
+            title="添加"
+            size="mini"
+            icon="el-icon-plus"
+            circle
+            @click="openEditForm(scope.row)"
+          ></el-button>
+          <el-button
+            v-if="scope.row.isAdd==1"
+            type="danger"
+            title="移除"
+            size="mini"
+            icon="el-icon-minus"
+            circle
+            @click="openEditForm(scope.row)"
+          ></el-button>
+        </template>
+      </el-table-column>
 
-            <el-table-column label="邮箱" prop="email" sortable="custom" :show-overflow-tooltip="true">
-                <template slot-scope="scope">
-                    {{scope.row.email}}
-                </template>
-            </el-table-column>
-            <el-table-column fixed="right" label="操作" align="center">
-                <template slot-scope="scope">
-
-                </template>
-            </el-table-column>
-
-        </el-table>
-        <div slot="footer">
-            <el-pagination :current-page="page.current" :page-size="page.size" :total="page.total" :page-sizes="[1,100, 200, 300, 400]" layout="total, sizes, prev, pager, next, jumper" style="margin: -10px;" @size-change="handleSizeChange" @current-change="handleCurrentChange">
-            </el-pagination>
-        </div>
-    </el-dialog>
+    </el-table>
+    <div slot="footer">
+      <el-pagination
+        :current-page="page.current"
+        :page-size="page.size"
+        :total="page.total"
+        :page-sizes="[1,100, 200, 300, 400]"
+        layout="total, sizes, prev, pager, next, jumper"
+        style="margin: -10px;"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      >
+      </el-pagination>
+    </div>
+  </el-dialog>
 </template>
 <script>
 import * as userService from "@/api/sys/user";
