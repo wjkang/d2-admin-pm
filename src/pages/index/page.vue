fbd7383402950e814b3a03a51c29939553f7d67d
wjkang <977865769@qq.com>
2019-01-02 13:35:11 +0800

    update

--- src/pages/index/page.vue
+++ src/pages/index/page.vue
@@ -1,34 +1,15 @@
 <template>
   <d2-container class="page">
     <d2-page-cover
-      :title="`D2 Admin ${version}`"
-      sub-title="优雅的中后台集成方案">
+      title="D2 Admin PM"
+      sub-title="基于D2 Admin实现的RBAC权限管理解决方案">
       <d2-icon-svg
         class="page__logo"
         name="d2-admin"/>
       <template slot="footer">
         <div class="page__btn-group">
-          <span @click="$open('https://github.com/d2-projects')">开源组织</span> |
-          <span @click="$open('https://doc.d2admin.fairyever.com/zh/')">文档</span> |
-          <span @click="$open('https://github.com/d2-projects/d2-admin-start-kit')">简化版</span> |
-          <span @click="$open('https://alibaba.github.io/ice/scaffold?type=vue')">飞冰</span> |
-          <span @click="$open('https://juejin.im/user/57a48b632e958a006691b946/posts')">掘金</span> |
-          <span @click="$open('https://awesome.fairyever.com/daily/')">开发者日报</span> |
-          <el-popover
-            :width="172"
-            trigger="hover">
-            <p class="d2-mt-0 d2-mb-10">D2Projects</p>
-            <img
-              src="./image/qr@2x.png"
-              style="width: 172px;">
-            <span slot="reference">微信公众号</span>
-            <p style="font-size: 12px; margin-top: 0px; margin-bottom: 0px;">
-              官方公众号，主要推送前端技术类文章、框架资源、学习教程，以及 D2 系列项目更新信息
-            </p>
-          </el-popover>
+          <span @click="$open('https://github.com/wjkang/d2-admin-pm')">Github</span>
         </div>
-        <d2-badge/>
-        <d2-help-btn/>
       </template>
     </d2-page-cover>
   </d2-container>
@@ -36,13 +17,7 @@
 
 <script>
 import { mapState } from 'vuex'
-import D2HelpBtn from './components/d2-help-btn'
-import D2Badge from './components/d2-badge'
 export default {
-  components: {
-    D2HelpBtn,
-    D2Badge
-  },
   computed: {
     ...mapState('d2admin/releases', [
       'version'
