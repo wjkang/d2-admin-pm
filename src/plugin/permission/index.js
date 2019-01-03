0270e369a323602be9019b1fcc83be3da18a4038
wjk <977865769@qq.com>
2019-01-03 23:08:15 +0800

    update

--- src/plugin/permission/index.js
+++ src/plugin/permission/index.js
@@ -3,14 +3,43 @@ export default {
         let store = options.store
         Vue.directive('permission', {
             bind: function (el, binding, vnode) {
+                let isAdmin = store.state.d2admin.permission.isAdmin;
+                if (isAdmin) {
+                    return
+                }
                 let needPermissions = binding.value;
                 let permissions = store.state.d2admin.permission.functions.concat(store.state.d2admin.permission.roles);
-                let isAdmin = store.state.d2admin.permission.isAdmin;
                 let hasPermission = permissions.some(s => {
                     return needPermissions.indexOf(s) > -1;
                 })
                 if (!hasPermission && !isAdmin) {
                     el.parentNode.removeChild(el);
+                }
+
+                let checkCodes = [];
+                if (binding.arg === "function") {
+                    checkCodes = USER_PERMISSION;
+                } else if (binding.arg === "role") {
+                    checkCodes = USER_ROLE;
+                } else {
+                    checkCodes = USER_PERMISSION.join(USER_ROLE);
+                }
+                let access = true;
+                if (binding.modifiers.all) {
+                    for (let need of binding.value) {
+                        if (!checkCodes.some(s => s === need)) {
+                            access = false;
+                            break;
+                        }
+                    }
+                } else {
+                    access = false;
+                    for (let need of binding.value) {
+                        if (checkCodes.some(s => s === need)) {
+                            access = true;
+                            break;
+                        }
+                    }
                 }
             }
         })
