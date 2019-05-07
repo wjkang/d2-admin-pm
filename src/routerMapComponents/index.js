1c434e12392a1e0cce99ace06eaea38ec4054f61
wjk <977865769@qq.com>
2019-05-08 01:19:14 +0800

    update

--- src/routerMapComponents/index.js
+++ src/routerMapComponents/index.js
@@ -1,9 +1,16 @@
+const files = require.context('./', true, /\.js$/);
 import layoutHeaderAside from '@/layout/header-aside'
-export default {
+
+let componentMaps = {
     "layoutHeaderAside": layoutHeaderAside,
     "menu": () => import(/* webpackChunkName: "menu" */'@/pages/sys/menu'),
     "route": () => import(/* webpackChunkName: "route" */'@/pages/sys/route'),
     "role": () => import(/* webpackChunkName: "role" */'@/pages/sys/role'),
     "user": () => import(/* webpackChunkName: "user" */'@/pages/sys/user'),
-    "interface": () => import(/* webpackChunkName: "interface" */'@/pages/sys/interface')
+    "interface": () => import(/* webpackChunkName: "interface" */'@/pages/sys/interface'),
 }
+files.keys().forEach((key) => {
+    if (key === './index.js') return
+    Object.assign(componentMaps, files(key).default)
+})
+export default componentMaps
