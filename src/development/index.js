252e49d65d5faf3b21fcfae9957b613d9183118d
wjk <977865769@qq.com>
2019-05-08 02:36:37 +0800

    update

---
+++ src/development/index.js
@@ -0,0 +1,17 @@
+const files = require.context('./', true, /\.js$/);
+
+let autoGenerateMenus = []
+let autoGenerateRouters = []
+files.keys().forEach((key) => {
+    console.log(key)
+    if (key === './index.js') return
+    if (key.indexOf('menu') > -1) {
+        autoGenerateMenus.push(files(key).default)
+    } else if (key.indexOf('router') > -1) {
+        autoGenerateRouters.push(files(key).default)
+    }
+})
+export default {
+    menus: autoGenerateMenus,
+    routers: autoGenerateRouters
+}
